2764e1c6c80e2c60109b16f2ace9e2647e4b7006
Artyom Vershilo <tema.v93@gmail.com>
2021-06-03 15:09:25 +0600

    Added entitties for database schema

---
+++ src/main/java/hamlet/catalog/entity/Characteristic.java
@@ -0,0 +1,64 @@
+package hamlet.catalog.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "characteristics")
+public class Characteristic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @OneToMany(mappedBy = "characteristic")
+    private List<Value> values;
+
+    private String title;
+
+    private String info;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public List<Value> getValues() {
+        return values;
+    }
+
+    public void setValues(List<Value> values) {
+        this.values = values;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
